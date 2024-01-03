cf5f0e09ef82607c9893e6685bf707f2e6cd2d60
ChiniMishri <gambhir.poudel@gmail.com>
2024-01-03 19:17:55 +0545

    Updated

--- lib/components/home.dart
+++ lib/components/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nepflix/components/appbar.dart';
 import 'package:nepflix/components/toprated.dart';
+import 'package:nepflix/components/topratedtv.dart';
 import 'package:nepflix/components/trending.dart';
 import 'package:nepflix/components/tv.dart';
 import 'package:nepflix/components/upcoming.dart';
@@ -18,6 +19,7 @@ class _HomeState extends State<Home> {
   List topratedmovies = [];
   List upcomingmovies = [];
   List tv = [];
+  List topratedtvs = [];
   final String apikey = 'd9e063e604b5c938e70876c537f01c20';
   final readaccesstoken =
       'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOWUwNjNlNjA0YjVjOTM4ZTcwODc2YzUzN2YwMWMyMCIsInN1YiI6IjY1OTM5MDU0NjUxZmNmNWViYThmYjFjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dvYTroDlmHiRcMd8kDpi5pBg7rCWW4P0EwrI0gOUPgw';
@@ -36,14 +38,16 @@ class _HomeState extends State<Home> {
     Map topratedresults = await tmdbWithCustomLogs.v3.movies.getTopRated();
     Map upcomingresults = await tmdbWithCustomLogs.v3.movies.getUpcoming();
     Map tvresults = await tmdbWithCustomLogs.v3.tv.getPopular();
+    Map topratedtvresults = await tmdbWithCustomLogs.v3.tv.getTopRated();
 
     setState(() {
       trendingmovies = trendingresults['results'];
       topratedmovies = topratedresults['results'];
       upcomingmovies = upcomingresults['results'];
       tv = tvresults['results'];
+      topratedtvs = topratedtvresults['results'];
     });
-    print(tv);
+    print(topratedtvs);
   }
 
   @override
@@ -61,9 +65,12 @@ class _HomeState extends State<Home> {
             PopularTV(
               tv: tv,
             ),
+            TopRatedTV(
+              topratedtv: topratedtvs,
+            ),
             Upcoming(
               upcoming: upcomingmovies,
-            )
+            ),
           ],
         ));
   }
